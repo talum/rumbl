feb0581c029268585ad27cad4529ff1c602f4c0c
talum <tracy.a.lum@gmail.com>
2017-09-10 14:58:20 -0400

    wip phoenix codealong

---
+++ web/models/video.ex
@@ -0,0 +1,40 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    field :slug, :string
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
+
+    timestamps()
+  end
+
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:url, :title, :description])
+    |> validate_required([:url, :title, :description])
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else 
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+  str
+  |> String.downcase()
+  |> String.replace(~r/[^w-]+/u, "-")
+  end
+end
